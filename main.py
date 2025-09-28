5a2828718101a9698084aa190a5ddf6ed10a41fd
EugeneSatan666 <msipacan@gmail.com>
2025-09-28 18:26:18 +0500

    I did the following check - if the user did not enter the first and last name, then it is displayed on the screen that it does not exist.

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 name = input("Whats your name?")
 lastname = input("Whats your lastname?")
-print("hello", name, lastname, "!")
+if name = "" and lastname = "":
+    print("You didn't enter your first and last name, so you don't exist.")
+else:
+    print("hello,", name, lastname, "!")
